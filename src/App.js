eee05c6dcb7b176c896d22e0b23384d5d6e047b7
Kundan Kumar <30737393+KcodeHub@users.noreply.github.com>
2019-05-25 16:26:58 +0530

    resume app with react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import $ from 'jquery';
 import './App.css';
-
+import Header from './Components/Header';
+import About from './Components/About';
+import Resume from './Components/Resume';
+import Portfolio from './Components/Portfolio';
+import Testimonials from './Components/Testimonials';
+import Contact from './Components/Contacts';
+import Footer from './Components/Footer';
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      foo: 'bar',
+      resumedata: {}
+    }
+  }
+  getResumeData(){
+    $.ajax({
+      url:'http://localhost:3000/resumedata.json',
+      dataType: 'json',
+      cache: false,
+      success: function(data){
+        this.setState({resumedata: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.log(err);
+        alert(err);
+      }
+
+    })
+  }
+
+  componentDidMount(){
+    this.getResumeData();
+  }
+
   render() {
+    console.log(this.state.resumedata);
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div >
+       <Header data={this.state.resumedata.main}/>
+       <About data={this.state.resumedata.main}/>
+       <Resume data={this.state.resumedata.resume}/>
+       <Portfolio data={this.state.resumedata.portfolio}/>
+       <Testimonials data={this.state.resumedata.testimonials}/>
+       <Contact data={this.state.resumedata.main}/>
+       <Footer />
       </div>
     );
   }
